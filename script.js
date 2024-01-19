14677cea9ac85f2c14430d887211969fc13c3196
jonathan-perez <jonathanibanez86@gmail.com>
2024-01-19 09:56:57 +0100

    boton

--- script.js
+++ script.js
@@ -51,3 +51,22 @@ function vibrate() {
     }
 }
 
+/*const boton = document.getElementById("info");
+
+boton.addEventListener("click", function(){
+    alert(". .-. . ... / . .-.. / ..-. ..- - ..- .-. --- / -.. . / .-.. .- / .--. .-. --- --. .-. .- -- .- -.-. .. --- -.");
+});*/
+
+function copiar(){
+    var origen = document.getElementById('target1');
+    var destino = document.getElementById('target2');
+    var copyFrom = document.createElement("textarea");
+    copyFrom.textContent = origen.value;
+    var body = document.getElementsByTagName('body')[0];
+    body.appendChild(copyFrom);
+    copyFrom.select();
+    document.execCommand('copy');
+    body.removeChild(copyFrom);
+    destino.focus();
+    document.execCommand('paste');
+  }
